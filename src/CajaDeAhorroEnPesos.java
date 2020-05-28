40afce903827d251b1299902903a470d537436b9
Tito <57116764+titodiazpierri@users.noreply.github.com>
2020-05-28 20:34:52 -0300

    pifie

--- src/CajaDeAhorroEnPesos.java
+++ src/CajaDeAhorroEnPesos.java
@@ -1,8 +1,8 @@
 
 public class CajaDeAhorroEnPesos extends CajaDeAhorro {
 
-	public CajaDeAhorroEnPesos(String alias, double saldo) {
-		super(1, alias, saldo);
+	public CajaDeAhorroEnPesos(byte tipoDeCuenta, String alias, double saldo) {
+		super(tipoDeCuenta, alias, saldo);
 		// TODO Apéndice de constructor generado automáticamente
 	}
 
